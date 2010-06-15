f10ace848e20d15d00d2f55ce65a46508adb0e11
Allen Rohner <arohner@gmail.com>
2010-06-14 21:11:32 -0500

    Make compojure a middleware

--- src/compojure/core.clj
+++ src/compojure/core.clj
@@ -29,7 +29,7 @@
          (route-compile ~route)
          ~route)))
 
-(defn- assoc-route-params
+(defn- merge-route-params
   "Associate route parameters with the request map."
   [request params]
   (merge-with merge request {:route-params params, :params params}))
@@ -42,33 +42,19 @@
           ~@(if more [more `(dissoc (~request :params) ~@(map keyword args))])]
        ~@body)))
 
-(defmacro bind-request
-  "Bind a request to a collection of symbols. The collection can be a Clojure
-  map destructuring binding for the request map, or it can be a vector of
-  parameter bindings."
-  [request bindings & body]
-  (if (vector? bindings)
-    (param-vector-bindings request bindings body)
-    `(let [~bindings ~request] ~@body)))
+(defmacro compile-route [method route bindings body]
+  (let [route (prepare-route route)
+        route-fn `(fn [~bindings] ~@body)]
+    `[~method ~route (quote ~bindings) ~route-fn]))
 
-(defn- compile-route
-  "Compile a route in the form (method path & body) into a function."
-  [method route bindings body]
-  `(let [route# ~(prepare-route route)]
-     (fn [request#]
-       (if (#'method-matches ~method request#)
-         (if-let [route-params# (route-matches route# request#)]
-           (let [request# (#'assoc-route-params request# route-params#)]
-             (bind-request request# ~bindings
-               (render request# (do ~@body)))))))))
+(defn combine-routes
+  ""
+  [& routes]
+  (apply concat routes))
 
-(defn routes
-  "Create a Ring handler by combining several handlers into one."
-  [& handlers]
-  (wrap-cookies
-    (wrap-params
-      (fn [request]
-        (some #(% request) handlers)))))
+(defn combine-handlers [ & handlers]
+  (fn [request]
+    (some #(% request) handlers)))
 
 (defn- apply-doc
   "Return a symbol and body with an optional docstring applied."
@@ -83,56 +69,38 @@
   [name doc? & routes]
   (let [[name & routes] (apply-doc name doc? routes)]
    `(def ~name
-      (routes ~@routes))))
+      (combine-routes ~@routes))))
 
-(defmacro GET "Generate a GET route."
-  [path args & body]
-  (compile-route :get path args body))
+(defn get-bindings [bindings request]
+  (if (vector? bindings)
+    (map #(or (get (request :params) (str %))
+              (get (request :params) (keyword %))) bindings)
+    nil))
 
-(defmacro POST "Generate a POST route."
-  [path args & body]
-  (compile-route :post path args body))
+(defn- matches? [request [method path bindings route-fn]]
+  (and (method-matches method request) (route-matches path request)))
 
-(defmacro PUT "Generate a PUT route."
-  [path args & body]
-  (compile-route :put path args body))
+(defn find-matching-route
+  "middleware that finds the matching route, if any, and adds it to the request"
+  [handler routes]
+  (fn [request]
+    (if-let [[method path bindings route-fn] (first (filter #(matches? request %) routes))]
+      (let [route-params (route-matches path request)
+            request (-> request
+                        (assoc :route-fn route-fn)
+                        (assoc :route-params route-params)
+                        (merge-route-params route-params)
+                        (assoc :route-bindings bindings))]
+        (handler request))
+      (handler request))))
 
-(defmacro DELETE "Generate a DELETE route."
-  [path args & body]
-  (compile-route :delete path args body))
-
-(defmacro HEAD "Generate a HEAD route."
-  [path args & body]
-  (compile-route :head path args body))
-
-(defmacro ANY "Generate a route that matches any method."
-  [path args & body]
-  (compile-route nil path args body))
-
-(defn- keyword->middleware
-  "Turn a keyword into a wrapper function symbol.
-  e.g. :test => wrap-test
-       (:test x) => (wrap-test x)"
-  [kw]
-  (letfn [(mw-sym [x]
-            (symbol (namespace x) (str "wrap-" (name x))))]
-    (cond
-      (keyword? kw)
-        (mw-sym kw)
-      (and (seq? kw) (keyword? (first kw)))
-        (cons (mw-sym (first kw)) (rest kw))
-      :else
-        kw)))
-
-(defmacro wrap!
-  "Wrap a handler in middleware functions. Uses the same syntax as the ->
-  macro. Additionally, keywords may be used to denote a leading 'wrap-'.
-  e.g.
-    (wrap! foo (:session cookie-store))
-    => (wrap! foo (wrap-session cookie-store))
-    => (def foo (wrap-session foo cookie-store))"
-  [handler & funcs]
-  (let [funcs (map keyword->middleware funcs)]
-    `(alter-var-root
-       (var ~handler)
-       (constantly (-> ~handler ~@funcs)))))
+(defn call-matching-route
+  "If the request has a matching route, calls it and returns the response, else continues calling the middleware chain"
+  [handler]
+  (fn [request]
+    (if (:route-fn request)
+      (render request
+              (if (:route-bindings request)
+                (apply (:route-fn request) (get-bindings (:route-bindings request) request))
+                ((:route-fn request) request)))
+      (handler request))))
